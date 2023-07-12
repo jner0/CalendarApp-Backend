c153341ba144dab43893007006354de3c2111227
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-07-11 21:46:22 -0500

    Adding events to DB

--- controllers/events.js
+++ controllers/events.js
@@ -1,39 +1,50 @@
-const {response} = require('express');
+const { response } = require("express");
+const Evento = require("../models/evento");
 
-const getEventos = async(req, res = response) => {
-    res.status(201).json({
-        ok: true,
-        msg: 'getEventos'
-    })
-}
+const getEventos = async (req, res = response) => {
+  res.status(201).json({
+    ok: true,
+    msg: "getEventos",
+  });
+};
 
-const crearEvento = async(req, res = response) => {
-    // verificar que tenga el evento
-    console.log( req.body );
+const crearEvento = async (req, res = response) => {
+  // verificar que tenga el evento
+  const evento = new Evento(req.body);
+  try {
+    evento.user = req.uid;
+    const eventoGuardado = await evento.save();
+    res.json({
+      ok: true,
+      evento: eventoGuardado,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Hable con el administrador",
+    });
+  }
 
-    res.status(201).json({
-        ok: true,
-        msg: 'crearEventos'
-    })
-}
+};
 
-const actualizarEvento = async( req, res = response) =>{
-    res.status(201).json({
-        ok: true,
-        msg: 'Actualizando Evento'
-    })
-}
+const actualizarEvento = async (req, res = response) => {
+  res.status(201).json({
+    ok: true,
+    msg: "Actualizando Evento",
+  });
+};
 
-const eliminarEvento = async( req, res = response) =>{
-    res.status(201).json({
-        ok: true,
-        msg: 'Eliminando Evento'
-    })
-}
+const eliminarEvento = async (req, res = response) => {
+  res.status(201).json({
+    ok: true,
+    msg: "Eliminando Evento",
+  });
+};
 
 module.exports = {
-    getEventos,
-    crearEvento,
-    actualizarEvento,
-    eliminarEvento
-}
+  getEventos,
+  crearEvento,
+  actualizarEvento,
+  eliminarEvento,
+};
