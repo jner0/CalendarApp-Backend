7650f1f5b5a941e7f84f1e2d0350d1ba3eebcf31
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-07-11 22:12:34 -0500

    Updating event

--- controllers/events.js
+++ controllers/events.js
@@ -33,10 +33,47 @@ const crearEvento = async (req, res = response) => {
 };
 
 const actualizarEvento = async (req, res = response) => {
-  res.status(201).json({
-    ok: true,
-    msg: "Actualizando Evento",
-  });
+
+    const eventoId = req.params.id;
+    const uid = req.uid;
+
+    try {
+        
+        const evento = await Evento.findById(eventoId);
+
+        if( !evento ){
+            res.status(404).json({
+                ok: false,
+                msg: 'Evento no existe por ese Id'
+            })
+        }
+
+        if(evento.user.toString() !== uid){
+            return res.status(401).json({
+                ok: false,
+                msg: 'No tiene privilegio de editar este evento'
+            })
+        }
+
+        const nuevoEvento = {
+            ...req.body,
+            user: uid
+        }
+
+        const eventoActualizado = await Evento.findByIdAndUpdate( eventoId, nuevoEvento, { new: true } );
+        res.json({
+            ok: true,
+            evento: eventoActualizado
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        })
+    }
+
 };
 
 const eliminarEvento = async (req, res = response) => {
